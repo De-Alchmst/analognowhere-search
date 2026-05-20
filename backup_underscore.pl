428033795b79b747fd29886241dc452502f39d0d
De-Alchmst <PolakJ_37@proton.me>
2026-05-20 14:13:06 +0200

    werks now...

--- backup_underscore.pl
+++ backup_underscore.pl
@@ -39,9 +39,10 @@ for my $link (@index) {
         $addr = "https://analognowhere.com/$link/";
         my $resp = get $addr;
 
+        # currently only the image itself present
         my @links = $resp =~ m/img src="(\S+?)"/;
-        # 0 = header image, 1+ = body posts
-        my $server_image_address = "https://analognowhere.com/$links[1]";
+
+        my $server_image_address = "https://analognowhere.com/$links[0]";
         my ($image_extention) = $server_image_address =~ m/(\.\w+)$/;
         $image_address = "backup/${link}/image$image_extention";
 
